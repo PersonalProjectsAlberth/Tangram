80b6590be1719a85c4c7313a4c0bd17a35644768
ALFAROSO <alberth.sotelo@gmail.com>
2025-04-19 16:35:03 -0700

    Display shapes from json file

--- src/app/shape-card/shape-card.component.ts
+++ src/app/shape-card/shape-card.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shape-card',
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './shape-card.component.html',
   styleUrl: './shape-card.component.css'
 })
-export class ShapeCardComponent {
+export class ShapeCardComponent implements OnInit{
+  shapes: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('/assets/shapes.json').subscribe((data) => {
+      this.shapes = data;
+    });
+  }
 }
