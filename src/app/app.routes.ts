abda7dd165afab4809f2f594924305f131caa17c
ALFAROSO <alberth.sotelo@gmail.com>
2025-04-19 15:48:06 -0700

    Shape card component implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ShapeCardComponent } from './shape-card/shape-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'shape', component: ShapeCardComponent},
+
+    {path: '**', redirectTo: 'shape', pathMatch: 'full'},
+];
---
+++ src/app/shape-card/shape-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shape-card',
+  imports: [],
+  templateUrl: './shape-card.component.html',
+  styleUrl: './shape-card.component.css'
+})
+export class ShapeCardComponent {
+
+}
