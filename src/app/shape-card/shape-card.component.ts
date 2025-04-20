9e1c8fd93155bec117d2ba44341d69c5899a05ac
ALFAROSO <alberth.sotelo@gmail.com>
2025-04-19 22:29:20 -0700

    Dark mode implemented and well display on scan component

--- src/app/shape-card/shape-card.component.ts
+++ src/app/shape-card/shape-card.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ThemeService } from '../services/theme.service';
 
 @Component({
   selector: 'app-shape-card',
@@ -10,13 +11,22 @@ import { Router } from '@angular/router';
   styleUrl: './shape-card.component.css'
 })
 export class ShapeCardComponent implements OnInit{
+
+  isDarkMode = true;
   shapes: any[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+  private themeService: ThemeService
+) {}
 
   ngOnInit(): void {
     this.http.get<any[]>('/assets/shapes.json').subscribe((data) => {
       this.shapes = data;
+    });
+
+    this.themeService.darkMode$.subscribe((isDarkMode) => {
+      this.isDarkMode = isDarkMode;
     });
   }
 
