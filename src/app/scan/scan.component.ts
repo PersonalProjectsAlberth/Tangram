0b22cd579f98048ceb4fb2164e7ea3ec7f0a02c2
ALFAROSO <alberth.sotelo@gmail.com>
2025-04-19 17:12:57 -0700

    Display correct shape from json file

--- src/app/scan/scan.component.ts
+++ src/app/scan/scan.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-scan',
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './scan.component.html',
-  styleUrl: './scan.component.css'
+  styleUrls: ['./scan.component.css']
 })
-export class ScanComponent {
+export class ScanComponent implements OnInit {
+  shapeId!: string;
+  shapePath: string | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+
+    this.shapeId = this.route.snapshot.paramMap.get('id') || '';
+
+    this.http.get<any[]>('/assets/shapes.json').subscribe((data) => {
+      const shape = data.find((item) => item.id === parseInt(this.shapeId, 10));
+      this.shapePath = shape ? shape.path : null;
+    });
+  }
 }
