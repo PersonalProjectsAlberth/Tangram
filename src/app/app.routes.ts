1f5fc1a57080508682d3b26705eb62af7d2af30a
ALFAROSO <alberth.sotelo@gmail.com>
2025-04-25 18:23:30 -0700

    Route implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { ShapeCardComponent } from './shape-card/shape-card.component';
 import { ScanComponent } from './scan/scan.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 export const routes: Routes = [
 
     {path: 'shape', component: ShapeCardComponent},
     {path: 'shape/:id', component: ScanComponent},
+    {path: 'statistics', component: StatisticsComponent},
 
     {path: '**', redirectTo: 'shape', pathMatch: 'full'},
 ];
