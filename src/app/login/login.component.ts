baac09b8fda964cd59275885e438e2f540a88fad
ALFAROSO <alberth.sotelo@gmail.com>
2025-04-30 11:10:59 -0700

    login ts file code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  username: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+  constructor( private router: Router) { }
+
+  onLogin(): void {
+    const validUsername = 'admin';
+    const validPassword = 'admin';
+
+    if (this.username === validUsername && this.password === validPassword) {
+      localStorage.setItem('isLoggedIn', 'true');
+      this.router.navigate(['/shape']);
+    } else {
+      this.errorMessage = 'Invalid username or password';
+    }
+  }
 
 }
