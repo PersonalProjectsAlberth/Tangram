2d2333998ef9cdcf038d956779522cc8cf7989ee
ALFAROSO <alberth.sotelo@gmail.com>
2025-04-30 11:12:03 -0700

    Route implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { ShapeCardComponent } from './shape-card/shape-card.component';
 import { ScanComponent } from './scan/scan.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { SettingsComponent } from './settings/settings.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
 
+    {path: 'login', component: LoginComponent },
     {path: 'shape', component: ShapeCardComponent},
     {path: 'shape/:id', component: ScanComponent},
     {path: 'statistics', component: StatisticsComponent},
     {path: 'settings', component: SettingsComponent},
 
-    {path: '**', redirectTo: 'shape', pathMatch: 'full'},
+    {path: '**', redirectTo: 'login', pathMatch: 'full'},
 ];
