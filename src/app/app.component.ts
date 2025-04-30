ff52d116733ae0017369901dc2c80d2059c9374e
ALFAROSO <alberth.sotelo@gmail.com>
2025-04-30 11:30:25 -0700

    auth service generated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageService } from './services/language.service';
@@ -8,17 +8,22 @@ import { LanguageService } from './services/language.service';
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Tangram-App';
 
   constructor(
     private translate: TranslateService,
-    private languageService: LanguageService)
-    {
+    private languageService: LanguageService,
+    private router: Router
+  ) {
     const savedLanguage = this.languageService.getLanguage();
     this.translate.use(savedLanguage);
   }
 
+  onLogout(): void {
+    localStorage.removeItem('isLoggedIn');
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
