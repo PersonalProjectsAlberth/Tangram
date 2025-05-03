24067c600e7c26086cd15b241f232f48e43c89b2
ALFAROSO <alberth.sotelo@gmail.com>
2025-05-02 21:52:47 -0700

    Lottie animation implemented

--- src/app/how-to-play/how-to-play.component.ts
+++ src/app/how-to-play/how-to-play.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { AnimationItem } from 'lottie-web';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-how-to-play',
-  imports: [],
+  imports: [LottieComponent],
   templateUrl: './how-to-play.component.html',
-  styleUrl: './how-to-play.component.css'
+  styleUrl: './how-to-play.component.css',
 })
 export class HowToPlayComponent {
 
+  animationItem: AnimationItem | null = null;
+  
+  options: AnimationOptions = {
+    path: '/lottie/howtoplay.json',
+    loop: false,
+    autoplay: true,
+  };
+
+  animationCreated(animationItem: AnimationItem): void {
+    this.animationItem = animationItem;
+  }
+
+  onAnimationClick(): void {
+    if (this.animationItem) {
+      this.animationItem.goToAndPlay(0, true);
+    }
+  }
 }
