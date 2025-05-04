0143c22038b3076ecdf1b94163d90c6e74f95a0d
ALFAROSO <alberth.sotelo@gmail.com>
2025-05-04 15:01:58 -0700

    Improve transition on sign in screen

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -15,13 +15,18 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
   errorMessage: string = '';
+  isVisible: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     if (this.authService.checkAuthentication()) {
       this.router.navigate(['/shape']);
-    }
+    };
+
+    setTimeout(() => {
+      this.isVisible = true;
+    }, 0);
   }
 
   onLogin(event: Event): void {
